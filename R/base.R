95ab36811a767b14c558698ea0a340bc3d838381
Yvon AWUKLU <72789628+yvoawk@users.noreply.github.com>
2021-08-13 03:56:06 +0200

    Update base.R

--- R/base.R
+++ R/base.R
@@ -1,10 +1,3 @@
-
-#' @export
-.apikey <- function() {
-    key
-  return(key)
-}
-
 #' @importFrom stringr str_detect
 .checkApikey <- function(apikey) {
   if (!is.character(apikey) ||
@@ -89,18 +82,6 @@
   ST <- rawToChar(query$content)
   .checkST(ST)
   return(ST)
-}
-
-#' @export
-#' @importFrom rvest read_html html_nodes html_table
-.loadVocabulary <- function() {
-  url <- "https://www.nlm.nih.gov/research/umls/sourcereleasedocs/index.html"
-  umls <- rvest::read_html(url)
-  table <- umls %>%
-    rvest::html_nodes(xpath = '//*[@id="example"]') %>%
-    rvest::html_table() %>%
-    .[[1]]
-  return(table)
 }
 
 .detect <- function(String) {
